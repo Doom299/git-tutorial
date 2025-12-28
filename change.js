d5e4add3c8d32a9a0f1f52cf20be2cd41560ee28
Himasara <himasara.dev@gmail.com>
2025-12-28 21:33:11 +0530

    Version 2

--- change.js
+++ change.js
@@ -1,1 +1,3 @@
 console.log("Change");
+console.log("Change11");
+console.log("Change123231");
--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 console.log("config.js");
-console.log("version1");
+console.log("version2 updated");
+console.log("version2 updated12");
+console.log("Change12wefw3231");
